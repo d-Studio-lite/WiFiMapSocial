175b9c5e61585e40f3aac920222f0071bb15af6d
evkopavel <catler@ukr.net>
2012-04-21 23:53:57 +0300

    adde files

---
+++ WiFi Map Social/WMOfflineMapView.h
@@ -0,0 +1,13 @@
+//
+//  WMOfflineMapView.h
+//  WiFi Map Social
+//
+//  Created by Apple on 21.04.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WMOfflineMapView : NSObject
+
+@end
