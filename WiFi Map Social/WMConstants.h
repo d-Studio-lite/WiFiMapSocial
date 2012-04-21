b3412bb7f691db534b0b76dda863ef44cc8ed22d
SergeRykovski <sergey.bebop@gmail.com>
2012-04-21 23:09:30 +0300

    added a constant for server URL

--- WiFi Map Social/WMConstants.h
+++ WiFi Map Social/WMConstants.h
@@ -10,3 +10,4 @@ static NSString *const kWMSpotNameKey = @"name";
 static NSString *const kWMSpotPasswordKey = @"password"; 
 static NSString *const kWMSpotLattitudeKey = @"lattitude"; 
 static NSString *const kWMSpotLongitudeKey = @"longitude"; 
+static NSString *const kWMServerURL = @"http://fierce-mountain-3562.herokuapp.com";
