d2de21637f87f4bb9c79dda11f450665cf6f8dd6
SergeRykovski <sergey.bebop@gmail.com>
2012-04-21 22:13:36 +0300

    Added constants and spot object

---
+++ WiFi Map Social/WMSpot.m
@@ -0,0 +1,13 @@
+//
+//  WMSpot.m
+//  WiFi Map Social
+//
+//  Created by Victoria Babakina on 21.04.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "WMSpot.h"
+
+@implementation WMSpot
+
+@end
