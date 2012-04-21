76a9cd2d32416f3c5acd0e983689189a02cf150e
evkopavel <catler@ukr.net>
2012-04-22 02:35:19 +0300

    impl

--- WiFi Map Social/WMMapViewSpotsAnnotation.h
+++ WiFi Map Social/WMMapViewSpotsAnnotation.h
@@ -12,10 +12,10 @@
 
 @interface WMMapViewSpotsAnnotation : NSObject <MKAnnotation>
 
-@property (nonatomic, readonly) CLLocationCoordinate2D coordinate;
+- (id)initWithSpotData:(WMSpotData *)spotData;
 
-//optional
-//@property (nonatomic, readonly, copy) NSString *title;
-//@property (nonatomic, readonly, copy) NSString *subtitle;
+@property (nonatomic, readonly) CLLocationCoordinate2D coordinate;
+@property (nonatomic, readonly, copy) NSString *title;
+@property (nonatomic, readonly, copy) NSString *subtitle;
 
 @end
