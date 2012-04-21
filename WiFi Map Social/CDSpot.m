17e0a91f85ecf9dedc2941a7c781562cf851a80d
Anton Mironov <antonvmironov@gmail.com>
2012-04-22 00:57:48 +0300

    Core Data added

---
+++ WiFi Map Social/CDSpot.m
@@ -0,0 +1,19 @@
+//
+//  CDSpot.m
+//  WiFi Map Social
+//
+//  Created by Оксана Фелештинская on 22.04.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "CDSpot.h"
+
+
+@implementation CDSpot
+
+@dynamic latitude;
+@dynamic longtitude;
+@dynamic name;
+@dynamic password;
+
+@end
---
+++ WiFi Map Social/Spot.h
@@ -0,0 +1,20 @@
+//
+//  Spot.h
+//  WiFi Map Social
+//
+//  Created by Оксана Фелештинская on 22.04.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface Spot : NSManagedObject
+
+@property (nonatomic, retain) NSNumber * latitude;
+@property (nonatomic, retain) NSNumber * longtitude;
+@property (nonatomic, retain) NSString * name;
+@property (nonatomic, retain) NSString * password;
+
+@end
