291ef595be572e887cf3088f32e31f2afeb0610b
evkopavel <catler@ukr.net>
2012-04-21 21:23:05 +0300

    addef iles

---
+++ WiFi Map Social/WMMapView.m
@@ -0,0 +1,13 @@
+//
+//  WMMapView.m
+//  WiFi Map Social
+//
+//  Created by Apple on 21.04.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "WMMapView.h"
+
+@implementation WMMapView
+
+@end
