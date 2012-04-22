7ebe1c3e3a03aa9e1c5bff5f2d236809f30ce586
evkopavel <catler@ukr.net>
2012-04-22 10:19:45 +0300

    dfdf

--- WiFi Map Social/WMMapMath.h
+++ WiFi Map Social/WMMapMath.h
@@ -5,3 +5,24 @@
 //  Created by Apple on 22.04.12.
 //  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 //
+
+#import <MapKit/MapKit.h>
+#import <CoreLocation/CLLocation.h>
+
+static const double WMMapMathDegreesPerPixel = 360.0 / 512.0;
+static const double WMMapMathPixelsPerDegree = 512.0 / 360.0;
+
+CLLocationDegrees validatedDegree(CLLocationDegrees degree)
+{
+    
+    while (degree > 180.0)
+    {
+        degree -= 360.0;
+    }
+    while (degree < -180.0)
+    {
+        degree += 360.0;
+    }
+    return degree;
+}
+
