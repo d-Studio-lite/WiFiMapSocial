cbf860a8a2a2f6e6f75543657baf62d05a10f587
SergeRykovski <sergey.bebop@gmail.com>
2012-04-22 08:06:30 +0300

    added like button (hopefully it is it)

---
+++ WiFi Map Social/FBLikeButton.h
@@ -0,0 +1,39 @@
+//
+//  FBLikeButton.h
+//  WiFi Map Social
+//
+//  Created by Victoria Babakina on 22.04.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+//FBLikeButton.h
+#define FB_LIKE_BUTTON_LOGIN_NOTIFICATION @"FBLikeLoginNotification"
+
+typedef enum {
+    FBLikeButtonStyleStandard,
+    FBLikeButtonStyleButtonCount,
+    FBLikeButtonStyleBoxCount
+} FBLikeButtonStyle;
+
+typedef enum {
+    FBLikeButtonColorLight,
+    FBLikeButtonColorDark
+} FBLikeButtonColor;
+
+@interface FBLikeButton : UIView {
+    
+    UIWebView *webView_;
+    
+    UIColor *textColor_;
+    UIColor *linkColor_;
+    UIColor *buttonColor_;
+}
+
+@property(retain) UIColor *textColor;
+@property(retain) UIColor *linkColor;
+@property(retain) UIColor *buttonColor;
+
+- (id)initWithFrame:(CGRect)frame andUrl:(NSString *)likePage andStyle:(FBLikeButtonStyle)style andColor:(FBLikeButtonColor)color;
+- (id)initWithFrame:(CGRect)frame andUrl:(NSString *)likePage;
+
+@end
