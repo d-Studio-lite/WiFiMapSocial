a8f237e3284bca9696cbf78ade07755d9b56df22
Anton Mironov <antonvmironov@gmail.com>
2012-04-22 03:26:03 +0300

    properties added

--- WiFi Map Social/WMDataController.h
+++ WiFi Map Social/WMDataController.h
@@ -8,6 +8,22 @@
 
 #import <Foundation/Foundation.h>
 
+@class WMSpotSource;
+
+@protocol WMDataContollerDelegate;
+
 @interface WMDataController : NSObject
+
+@property (retain, nonatomic, readonly) WMSpotSource *spotSource;
+@property (assign, nonatomic) id<WMDataContollerDelegate> delegate;
+
+- (void)update;
+
+@end
+
+
+@protocol WMDataContollerDelegate <NSObject>
+
+- (void)dataController:(WMDataController *)dataController updateDidFinishedWithError:(NSError *)error;
 
 @end
--- WiFi Map Social/WMDataController.m
+++ WiFi Map Social/WMDataController.m
@@ -7,7 +7,39 @@
 //
 
 #import "WMDataController.h"
+#import "WMSpotSource.h"
+
+@interface WMDataController()
+
+@property (retain, nonatomic) WMSpotSource *spotSource;
+
+@end
 
 @implementation WMDataController
+
+@synthesize spotSource = _spotSource;
+@synthesize delegate = _delegate;
+
+- (id)init
+{
+    self = [super init];
+    if (nil != self)
+    {
+        self.spotSource = [[[WMSpotSource alloc] init] autorelease];
+    }
+    return self;
+}
+
+- (void)dealloc
+{
+    self.spotSource = nil;
+    self.delegate = nil;
+    [super dealloc];
+}
+
+- (void)update
+{
+    //[[self spotSource] update];
+}
 
 @end
